ec2bd61b2baaf5a0355ed22d50308e964c15b6c1
Jessy Yeh <jiejun.yeh@gmail.com>
2024-05-13 13:38:19 +0100

    refactor code

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,12 @@
 import "./App.css";
-import List from "./components/List/List";
+import Board from "./components/Board/Board";
 
 function App() {
   return (
     <>
       <h1>Trello</h1>
       <h2>Project Management</h2>
-      <div className="lists">
-        <List title="Project Resources" />
-        <List title="To Do" />
-      </div>
+      <Board />
     </>
   );
 }
