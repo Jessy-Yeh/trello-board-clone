bed6f0aa6ead956846270576b6c4ba97a7c80341
Jessy Yeh <jiejun.yeh@gmail.com>
2024-05-17 10:25:01 +0100

    Add bin icon to Card

--- front-end/src/components/Card/Card.tsx
+++ front-end/src/components/Card/Card.tsx
@@ -1,11 +1,19 @@
 import { AddCardType } from "../../types";
+import bin from "../../assets/bin.svg";
+import styles from "./Card.module.css";
 
 interface Props {
   card: AddCardType;
 }
 
 const Card = ({ card }: Props) => {
-  return <li>{card.title}</li>;
+  return (
+    <div className={styles.cardGroup}>
+      {/* todo: make overflow text go to the next line */}
+      <li>{card.title}</li>
+      <img className={styles.bin} src={bin} />
+    </div>
+  );
 };
 
 export default Card;
