d4f7aa7363fcb23e18a398104dd13cc7f1829ecd
Jessy Yeh <jiejun.yeh@gmail.com>
2024-05-17 09:30:33 +0100

    add id to type and create new types for frontend

--- front-end/src/types/index.ts
+++ front-end/src/types/index.ts
@@ -1,14 +1,31 @@
 export type CardType = {
+  id: string;
+  title: string;
+  label: string;
+};
+export type AddCardType = {
   title: string;
   label: string;
 };
 
 export type ListType = {
+  id: string;
+  title: string;
+  cards: CardType[];
+};
+
+export type AddListType = {
   title: string;
   cards: CardType[];
 };
 
 export type BoardType = {
+  id: string;
+  title: string;
+  lists: ListType[];
+};
+
+export type CreateBoardType = {
   title: string;
   lists: ListType[];
 };
