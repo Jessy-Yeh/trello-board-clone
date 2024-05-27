958fb351ba934a34e45bcb3609c3d32adbd114f5
Jessy Yeh <jiejun.yeh@gmail.com>
2024-05-27 15:00:33 +0100

    hard coded ids for initial dummy data

--- back-end/dummyData/index.js
+++ back-end/dummyData/index.js
@@ -1,32 +1,39 @@
-import { v4 as uuidv4 } from "uuid";
-
 export const initialBoard = {
-  id: uuidv4(),
+  id: "26087739-d2a0-4809-95e1-2e9449a8fae0",
   title: "test",
   lists: [
     {
-      id: uuidv4(),
+      id: "7b694465-7221-471f-8b14-8849f7ce5b55",
       title: "to do",
       cards: [
         {
-          id: uuidv4(),
+          id: "4189af3d-e68e-43aa-9b41-d39dfed9fefc",
           title: "folding clothes",
           label: "important",
         },
-        { id: uuidv4(), title: "washing dishes" },
+        {
+          id: "512b1385-14b9-49a6-a522-985856904f63",
+          title: "washing dishes",
+        },
       ],
     },
     {
-      id: uuidv4(),
+      id: "87f3ee38-95f1-4dd1-a654-e6f06cb0bfad",
       title: "questions",
       cards: [
         {
-          id: uuidv4(),
+          id: "76269dbb-3a36-4123-b010-6b4259454a1c",
           title: "Who is bao?",
           label: "important",
         },
-        { id: uuidv4(), title: "where is mao?" },
-        { id: uuidv4(), title: "rrrrrr" },
+        {
+          id: "56ba0c2f-e495-4720-8a76-770b7a871fac",
+          title: "where is mao?",
+        },
+        {
+          id: "267ff5a1-d6a5-4800-9396-67ea276b8c38",
+          title: "rrrrrr",
+        },
       ],
     },
   ],
